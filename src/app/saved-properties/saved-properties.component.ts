6eb9fbf05201b4aeb13b4d0e54afdf3e76953073
dusan233 <dusan.jovanovicno.4@gmail.com>
2021-02-19 17:31:28 +0100

    saved properties page finished

--- src/app/saved-properties/saved-properties.component.ts
+++ src/app/saved-properties/saved-properties.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Property } from '../homes/home-model';
+import { HomesService } from '../homes/homes.service';
 
 @Component({
   selector: 'app-saved-properties',
@@ -7,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SavedPropertiesComponent implements OnInit {
 
-  constructor() { }
+  savedProperties: Property[] = [];
+
+  constructor(private propertiesService: HomesService) { }
 
   ngOnInit(): void {
+    this.savedProperties = this.propertiesService.getSavedProperties();
+
+    this.propertiesService.propertySavedRemoved.subscribe(property => {
+      const elIndex = this.savedProperties.findIndex(prop => prop.property_id === property.property_id)
+      if(elIndex === -1) {
+        this.savedProperties.push(property)
+      }else {
+        this.savedProperties = this.savedProperties.filter(prop => prop.property_id !== property.property_id);
+      }
+    })
   }
 
 }
